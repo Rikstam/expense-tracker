0728453408796b6314c8c1f48bf215dad8897bda
Riku <rikukestila@gmail.com>
2025-05-15 11:32:25 +0300

    Category icons and date formatting

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,4 +1,8 @@
 import 'package:uuid/uuid.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
@@ -9,12 +13,23 @@ enum Category {
   work,
 }
 
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
+
 class Expense {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 
   Expense({
     required this.title,
